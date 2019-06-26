55bd6cd4f5f57d4e3ccaedbfd04d1b7e115fd8c2
Eric <ericrodrigo999@gmail.com>
2019-06-26 16:06:12 -0300

    Initial Commit

--- blocos/footer.php
+++ blocos/footer.php
@@ -1,5 +1,9 @@
 
-
+<script language="Javascript">
+window.onload = function () {
+setTimeout('location.reload();', 10000);
+}
+</script>
  		<script src="../assets/js/jquery.js"></script>
  		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 		<script src="../assets/js/custom.js"></script>
